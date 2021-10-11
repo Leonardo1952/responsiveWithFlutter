7a787731b58fdc2991c4489f1794704f4783233c
Leonardo1952 <lo88980@gmail.com>
2021-10-11 09:01:49 -0300

    top to tablet

--- lib/pages/home/widgets/sections/top_section.dart
+++ lib/pages/home/widgets/sections/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:interface_responsiva/breakpoints.dart';
 import 'package:interface_responsiva/pages/home/widgets/custom_search_field.dart';
 
 class TopSection extends StatelessWidget {
@@ -57,6 +58,57 @@ class TopSection extends StatelessWidget {
             ),
           );
 
+        if (maxWidth >= mobileBreakPoint) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network(
+                    'https://images.pexels.com/photos/892757/pexels-photo-892757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      width: 350,
+                      padding: const EdgeInsets.all(22),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Aprenda Flutter com este curso',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Bora aprender Flutter com o prof Leonardo Oliveira! Cursos por apenas R\$19,99. Qualidade garantida.',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          CustomSearchField()
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
         return Container();
       },
     );
